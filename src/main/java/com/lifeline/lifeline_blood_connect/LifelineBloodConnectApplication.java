f1c3ee56173849692b2a988d825e62b53a12f712
nitesh129 <Niteshsharma2698@gmail.com>
2026-09-18 14:09:54 +0530

    Initial commit..

---
+++ src/main/java/com/lifeline/lifeline_blood_connect/LifelineBloodConnectApplication.java
@@ -0,0 +1,16 @@
+package com.lifeline.lifeline_blood_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class LifelineBloodConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelineBloodConnectApplication.class, args);
+
+		System.out.println("Lifeline Blood Connect Application Started");
+	}
+
+}
---
+++ src/test/java/com/lifeline/lifeline_blood_connect/LifelineBloodConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeline.lifeline_blood_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelineBloodConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
